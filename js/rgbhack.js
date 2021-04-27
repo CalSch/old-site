ac08490e020c21ef9410c85fa2a32a544c074cc9
CalSch <35741152+CalSch@users.noreply.github.com>
2021-04-27 15:41:09 -0500

    Create rgbhack.js

---
+++ js/rgbhack.js
@@ -0,0 +1,6 @@
+let hackMenuContent=`<h3>Hack Menu</h3>`;
+let hackMenu=document.createElement("div")
+hackMenu.id="hack-menu";
+hackMenu.innerHTML=hackMenuContent;
+
+document.appendChild(hackMenu);
