76a5d1b2e0d523aaa0d73cea308d511f74fd3918
CalSch <35741152+CalSch@users.noreply.github.com>
2021-04-27 15:42:08 -0500

    Update rgbhack.js

--- js/rgbhack.js
+++ js/rgbhack.js
@@ -1,6 +1,9 @@
-let hackMenuContent=`<h3>Hack Menu</h3>`;
-let hackMenu=document.createElement("div")
-hackMenu.id="hack-menu";
-hackMenu.innerHTML=hackMenuContent;
+try{
+  let hackMenuContent=`<h3>Hack Menu</h3>`;
+  let hackMenu=document.createElement("div")
+  hackMenu.id="hack-menu";
+  hackMenu.innerHTML=hackMenuContent;
 
-document.appendChild(hackMenu);
+  document.appendChild(hackMenu);
+}
+catch(e) alert("Error: "+e.message)
